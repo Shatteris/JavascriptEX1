348f54a2a615cf9ad74dc12abf534f7b56d156c0
Shatteris <114663123+Shatteris@users.noreply.github.com>
2022-11-20 17:28:18 +0000

    Add files via upload

---
+++ Ex_JScript.js
@@ -0,0 +1,35 @@
+
+
+let nameH = "Hugo";
+let ageH = 28;
+let ageR=28+7;
+
+let friends = ["João","Maria","Inês"]
+
+let Hperson ={
+    name:"Hugo",
+    age:28,
+    isStudent:true,
+};
+
+let friendships = [
+    {
+        name:"João",
+        age:27,
+        isStudent:false,
+    },
+
+    {
+        name:"Maria",
+        age:27,
+        isStudent:false,
+    },
+
+    {
+        name:"Inês",
+        age:27,
+        isStudent:false,
+    },
+    ]
+    
+    console.log(nameH,ageH,ageR,friends,Hperson,friendships);
